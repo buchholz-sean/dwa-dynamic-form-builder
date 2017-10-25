4fed480bd6b830fe4116cfad4e5f98452c9cd04a
Sean Buchholz <smbuchholz@fullsail.edu>
2017-10-25 17:20:53 -0400

    Fix DWA Style Guide compliance

--- src/demo.js
+++ src/demo.js
@@ -1,36 +1,37 @@
 const tool = require('./tool.js');
 
-var demoArr = [{
-    type: 'label',
-    id: 'nameLabel',
-    for: 'nameInput'
+const demoArr = [{
+  type: 'label',
+  id: 'nameLabel',
+  for: 'nameInput',
 }, {
-    type: 'text',
-    id: 'nameInput',
-    required: true
+  type: 'text',
+  id: 'nameInput',
+  required: true,
 }, {
-    type: 'SELECT',
-    id: 'caseInsensitive'
+  type: 'SELECT',
+  id: 'caseInsensitive',
 }, {
-    type: 'select',
-    for: 'bogusProperty',
-    required: false
+  type: 'select',
+  for: 'bogusProperty',
+  required: false,
 }, {
-    type: 'bogusInput',
-    class: 'anotherBogusProp',
-    id: 'this won\'t render'
+  type: 'bogusInput',
+  class: 'anotherBogusProp',
+  id: 'this won\'t render',
 }, {
-    type: 'text',
-    class: 'textClass',
-    required: 'true'
+  type: 'text',
+  class: 'textClass',
+  required: 'true',
 }, {
-    foo: 'bar',
-    baz: 'bat'
+  foo: 'bar',
+  baz: 'bat',
 }, {
-    type: 'Submit',
-    id: 'submitBtn',
-    formaction: '/some/action.php',
-    value: 'Submit Form'
+  type: 'Submit',
+  id: 'submitBtn',
+  formaction: '/some/action.php',
+  value: 'Submit Form',
 }];
 
+// eslint-disable-next-line no-console
 console.log(tool.genForm(demoArr));
--- src/tool.js
+++ src/tool.js
@@ -8,64 +8,69 @@ exports.genForm = (inputs) => {
         // Switch expression handles accepted input `type` props.
         // toLowerCase makes sure we don't have to handle case sensitivity
         switch (input.type.toLowerCase()) {
-          // Ternary expressions in the middle of each case add context if user provides optional `id` or `for` properties
+          // Ternary expressions in the middle of each case add context
+          // if user provides optional `id` or `for` properties
           case 'text':
           case 'password':
           case 'email':
-            formStr += `<input type="${input.type}"` +
-              (input.id ? ` id="${input.id}"` : ``) +
-              (input.class ? ` class="${input.class}"` : ``) +
-              (input.name ? ` name="${input.name}"` : ``) +
-              (input.form ? ` form="${input.form}"` : ``) +
-              (input.value ? ` value="${input.value}"` : ``) +
-              (input.required ? ` required` : ``) +
-              ` />`;
+            formStr += `<input type="${input.type}"${
+              input.id ? ` id="${input.id}"` : ''
+            }${input.class ? ` class="${input.class}"` : ''
+            }${input.name ? ` name="${input.name}"` : ''
+            }${input.form ? ` form="${input.form}"` : ''
+            }${input.value ? ` value="${input.value}"` : ''
+            }${input.required ? ' required' : ''
+            } />`;
             break;
           case 'label':
-            formStr += `<label` +
-            (input.id ? ` id="${input.id}"` : ``) +
-            (input.class ? ` class="${input.class}"` : ``) +
-            (input.for ? ` for="${input.for}"` : ``) +
-            (input.form ? ` form="${input.form}"` : ``) +
-            `></label>`;
+            formStr += `<label${
+              input.id ? ` id="${input.id}"` : ''
+            }${input.class ? ` class="${input.class}"` : ''
+            }${input.for ? ` for="${input.for}"` : ''
+            }${input.form ? ` form="${input.form}"` : ''
+            }></label>`;
             break;
           case 'select':
-            formStr += `<select` +
-            (input.id ? ` id="${input.id}"` : ``) +
-            (input.class ? ` class="${input.class}"` : ``) +
-            (input.name ? ` name="${input.name}"` : ``) +
-            (input.form ? ` form="${input.form}"` : ``) +
-            (input.required ? ` required` : ``) +
-            `></select>`;
+            formStr += `<select${
+              input.id ? ` id="${input.id}"` : ''
+            }${input.class ? ` class="${input.class}"` : ''
+            }${input.name ? ` name="${input.name}"` : ''
+            }${input.form ? ` form="${input.form}"` : ''
+            }${input.required ? ' required' : ''
+            }></select>`;
             break;
           case 'reset':
-            formStr += `<input type="reset"` +
-              (input.id ? ` id="${input.id}"` : ``) +
-              (input.class ? ` class="${input.class}"` : ``) +
-              (input.name ? ` name="${input.name}"` : ``) +
-              (input.form ? ` form="${input.form}"` : ``) +
-              (input.value ? ` value="${input.value}"` : ``) +
-              ` />`;
+            formStr += `<input type="reset"${
+              input.id ? ` id="${input.id}"` : ''
+            }${input.class ? ` class="${input.class}"` : ''
+            }${input.name ? ` name="${input.name}"` : ''
+            }${input.form ? ` form="${input.form}"` : ''
+            }${input.value ? ` value="${input.value}"` : ''
+            } />`;
             break;
           case 'submit':
-            formStr += `<input type="submit"` +
-              (input.id ? ` id="${input.id}"` : ``) +
-              (input.class ? ` class="${input.class}"` : ``) +
-              (input.name ? ` name="${input.name}"` : ``) +
-              (input.form ? ` form="${input.form}"` : ``) +
-              (input.formaction ? ` formaction="${input.formaction}"` : ``) +
-              (input.value ? ` value="${input.value}"` : ``) +
-              ` />`;
+            formStr += `<input type="submit"${
+              input.id ? ` id="${input.id}"` : ''
+            }${input.class ? ` class="${input.class}"` : ''
+            }${input.name ? ` name="${input.name}"` : ''
+            }${input.form ? ` form="${input.form}"` : ''
+            }${input.formaction ? ` formaction="${input.formaction}"` : ''
+            }${input.value ? ` value="${input.value}"` : ''
+            } />`;
             break;
           default:
-          // If input doesn't match a supported value for `type`, we don't add anything to formStr
+          // If input doesn't match a supported value for `type`, we don't
+          // add anything to formStr
+
+          // eslint-disable-next-line no-console
             console.log(`Invalid type: ${input.type}`);
         }
       }
-    })
+    });
   } catch (e) {
-    console.log('Error: ' + e);
-  };
+    // eslint-disable-next-line no-console
+    console.log(`Error: ${e}`);
+  }
   // Send back the completed output string
   return formStr;
-}
+};
